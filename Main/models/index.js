138c64ebdd99c3b3eb01934898d77e4e576a30de
James Garinger <jkggaringer@gmail.com>
2021-11-02 08:30:59 -0500

    adding base folders and naming conventions. Views, public, models, api routes.

---
+++ Main/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Project = require('./Project');
+const Comments = require('./comments');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project };
---
+++ Main/seeds/seed.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User, Project } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
+const commentsData = require('./commentsData.json');
+const Comments = require('../models/comments');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comments of commentsData) {
+    await Comments.create({
+      ...comments,
+      id = 1
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
